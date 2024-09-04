f1cf7859085397aa393e845a1b169144dd5a1a45
LegendXYT <46520558+LegendXYT@users.noreply.github.com>
2024-09-05 09:28:22 +1000

    Added Ethan in script.js

--- myscript.js
+++ myscript.js
@@ -48,7 +48,31 @@ let students =
 		"path": "amberle",
 		"lastName": "Seidl",
 		"firstName": "Amberle"
+<<<<<<< Updated upstream
 	}    
+=======
+	},
+	{
+		"path": "JayPanupong",
+		"lastName": "Jangjun",
+		"firstName": "Panupong"
+	},
+	{
+		"path": "rogan",
+		"lastName": "Cavanaugh",
+		"firstName": "Rogan"
+	},
+	{
+		"path": "Sofia",
+		"lastName": "Mironenko",
+		"firstName": "Sofia"
+	},
+	{
+		"path": "ethan",
+		"lastName": "Rechichi",
+		"firstName": "Ethan"
+	}
+>>>>>>> Stashed changes
 ];
 
 const maxColor = 15;
