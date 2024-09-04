47d7d1e23fa240e11efec9f0a5ceb462d14a93f5
RobotQuest <149337152+RobotQuest@users.noreply.github.com>
2024-09-05 09:49:48 +1000

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -48,7 +48,40 @@ let students =
 		"path": "amberle",
 		"lastName": "Seidl",
 		"firstName": "Amberle"
+<<<<<<< Updated upstream
 	}    
+=======
+	},
+	{
+		"path": "JayPanupong",
+		"lastName": "Jangjun",
+		"firstName": "Panupong"
+	},
+	{
+		"path": "rogan",
+		"lastName": "Cavanaugh",
+		"firstName": "Rogan"
+	},
+	{
+		"path": "Sofia",
+		"lastName": "Mironenko",
+		"firstName": "Sofia"
+	},
+	{
+		"path": "ethan",
+		"lastName": "Rechichi",
+		"firstName": "Ethan"
+	},
+	{
+		"path": "Hayley",
+		"lastName": "Searle",
+		"firstName": "Hayley"
+	},{
+		"path":"jeremy",
+		"lastName":"Johnston",
+		"firstName":"Jeremy"
+	}
+>>>>>>> Stashed changes
 ];
 
 const maxColor = 15;
