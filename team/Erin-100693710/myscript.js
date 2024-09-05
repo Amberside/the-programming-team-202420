151d76c42536d5168a31944e948de0d1ce0bae71
ezza-saund <eza.saunders@gmail.com>
2024-09-05 12:51:06 +1000

    formatting Slide show
    - slideshow is visible
    - attempting to adjust the top photo size
    - attemping to add flex command to .row thumbnails so all images are visible in the thumbnails at bottom of slide show

--- team/Erin-100693710/myscript.js
+++ team/Erin-100693710/myscript.js
@@ -13,7 +13,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName("photos");
+  let slides = document.getElementsByClassName("ss-image");
   let dots = document.getElementsByClassName("demo");
   let captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
